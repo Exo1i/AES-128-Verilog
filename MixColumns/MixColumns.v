f6a7eb5300c2d551b201bccded50ded8bb690b69
Mohamed Bahgat <mbahgat503@gmail.com>
2024-04-17 00:01:52 +0200

    MixColumns1.1

--- MixColumns/MixColumns.v
+++ MixColumns/MixColumns.v
@@ -1,9 +1,10 @@
 module MixColumns(in,out);
 
-    input [3:0] in;
-    output [3:0]out;
+  	input [31:0] in;
+  output [31:0]out;
     reg [15:0] Array [3:0][3:0];
-    initial begin
+	 
+	initial begin
         Array[0][0] =  8'h02;
         Array[0][1] =  8'h03;
         Array[0][2] =  8'h01;
@@ -23,12 +24,12 @@ module MixColumns(in,out);
         Array[3][1] =  8'h01;
         Array[3][2] =  8'h01;
         Array[3][3] =  8'h03;
-    end
-
-    assign out[0]  = (Array[0][0] & in[0]) ^ (Array[0][1] & in[1]) ^ (Array[0][2] & in[2]) ^ (Array[0][3] & in[3]);
-    assign out[1]  = (Array[1][0] & in[0]) ^ (Array[1][1] & in[1]) ^ (Array[1][2] & in[2]) ^ (Array[1][3] & in[3]);
-    assign out[2]  = (Array[2][0] & in[0]) ^ (Array[2][1] & in[1]) ^ (Array[2][2] & in[2]) ^ (Array[2][3] & in[3]);
-    assign out[3]  = (Array[3][0] & in[0]) ^ (Array[3][1] & in[1]) ^ (Array[3][2] & in[2]) ^ (Array[3][3] & in[3]);
+	end
+	
+  assign out[7:0]    = (Array[0][0] & in[7:0])   ^ (Array[0][1] & in[7:0]) ^  (Array[0][2] & in[7:0]) ^  (Array[0][3] & in[7:0]);
+  assign out[15:8]   = (Array[1][0] & in[15:8])  ^ (Array[1][1] & in[15:8]) ^ (Array[1][2] & in[15:8]) ^ (Array[1][3] & in[15:8]);
+  assign out[23:16]  = (Array[2][0] & in[23:16]) ^ (Array[2][1] & in[23:1]) ^ (Array[2][2] & in[23:1]) ^ (Array[2][3] & in[23:1]);
+  assign out[31:24]  = (Array[3][0] & in[31:24]) ^ (Array[3][1] & in[31:2]) ^ (Array[3][2] & in[31:2]) ^ (Array[3][3] & in[31:2]);
 
 endmodule
 
