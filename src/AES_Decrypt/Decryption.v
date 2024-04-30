63e02a32dee810592424f5c5b96843ed5c70d38c
Mohamed Bahgat <mbahgat503@gmail.com>
2024-04-30 22:44:27 +0300

    See
    ddd

--- src/AES_Decrypt/Decryption.v
+++ src/AES_Decrypt/Decryption.v
@@ -1,20 +1,20 @@
 module Decryption#(parameter Nr=14,parameter Nk=8)(clk,plain_text,key,cipher_text);
   input clk;
-  input wire[0:127] plain_text;
-  input wire [0:32*Nk-1] key;
-  output [0:127] cipher_text;
-  wire [0:128*Nr-1] fullkeys;
-  wire [0:127] Instates;
-  reg [0:127] InstatesReg = 0;
+  input wire[127:0] plain_text;
+  input wire [32*Nk-1:0] key;
+  output [127:0] cipher_text;
+  wire [128*Nr-1:0] fullkeys;
+  wire [127:0] Instates;
+  reg [127:0] InstatesReg = 0;
   assign Instates = InstatesReg;
 
-  wire [0:127] Outstates;
+  wire [127:0] Outstates;
 
-  wire [0:127] subBytesFinalStateWire;
-  reg [0:127] subBytesFinalStateReg;
+  wire [127:0] subBytesFinalStateWire;
+  reg [127:0] subBytesFinalStateReg;
   assign subBytesFinalStateWire = subBytesFinalStateReg;
-  wire [0:127] afterSubBFinal;
-  wire [0:127] afterShiftRowsFinal;
+  wire [127:0] afterSubBFinal;
+  wire [127:0] afterShiftRowsFinal;
   integer roundNum=0;
   keySchedule #(Nr,Nk) ke (key,fullkeys);
 
@@ -32,7 +32,7 @@ module Decryption#(parameter Nr=14,parameter Nk=8)(clk,plain_text,key,cipher_tex
   begin
     if(roundNum ==0)
     begin
-      InstatesReg=plain_text^key[0:127];
+      InstatesReg=plain_text^key[127:0];
       roundNum=roundNum+1;
     end
     else if(roundNum <Nr-1)
